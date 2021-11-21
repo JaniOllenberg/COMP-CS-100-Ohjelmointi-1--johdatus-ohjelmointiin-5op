ab36464c902a5453704199e6a193e7b9f397e594
Jani Ollenberg <jani.ollenberg@gmail.com>
2021-11-21 08:56:07 +0200

    credit calculator

--- Week_3_Functions/3.3 Study Credit calculator.py
+++ Week_3_Functions/3.3 Study Credit calculator.py
@@ -10,21 +10,23 @@ def main ():
     credits_sum = 0
     zero_credits_in_a_row = 0
     for i in range(1, months + 1):
-        credits = int(input(f"Enter the number of credits in month {i} "))
+        credits = int(input(f"Enter the number of credits in month {i}: "))
         credits_sum += credits
         if credits == 0:
             zero_credits_in_a_row += 1
         else:
             zero_credits_in_a_row = 0
         if zero_credits_in_a_row == 2:
+            print()
             print("You did have too many study breaks!")
             break
     if zero_credits_in_a_row != 2:
+        print()
         average_credits = credits_sum / months
         if average_credits >= 5.5:
-            print(f"You are a full time student and your monthly credit point average is {average_credits}")
+            print(f"You are a full time student and your monthly credit point average is {average_credits:.1f}.")
         else:
-            print(f'Your monthly credit point average {average_credits} does not classify you as a full time student.')
+            print(f'Your monthly credit point average {average_credits:.1f} does not classify you as a full time student.')
 
 if __name__ == "__main__":
     main()
