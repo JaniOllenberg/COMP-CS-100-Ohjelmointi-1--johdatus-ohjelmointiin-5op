8bc5cef024bc7b585b1ede76e087d3b15fc87246
Jani Ollenberg <jani.ollenberg@gmail.com>
2021-11-23 20:18:16 +0200

    triangle angles

---
+++ Week_4_More_Functions/4.10.1 Triangle_angles.py
@@ -0,0 +1,14 @@
+"""
+COMP.CS.100 Programming 1
+Jani Ollenberg
+H288244
+"""
+
+def calculate_angle(a, b=90):
+    """
+    Triangle angle calculator.
+    :param: a, angle
+    :param b: second angle, default 90 degrees
+    :return: 3rd angle
+    """
+    return 180 - a - b
--- Week_4_More_Functions/4.8 geometric_patterns.py
+++ Week_4_More_Functions/4.8 geometric_patterns.py
@@ -1,7 +1,73 @@
 """
 COMP.CS.100 Programming 1
 Code template for geometric patterns.
+Jani Ollenberg
+H288244
 """
+import math
+
+def circle():
+    """
+    calculate circle
+    :param: none
+    :return: none
+    """
+    radius = ask_side("Enter the circle's radius: ")
+    print(f"The circumference is {circumference_circle(radius):.2f}")
+    print(f"The surface area is {area(radius):.2f}")
+
+def circumference_circle(radius):
+    """
+    Calculate circle circumference.
+    :param: radius
+    :return: circumference
+    """
+    return math.pi * radius * 2
+
+def area(radius):
+    """
+    Calculate circle area.
+    :param: radius
+    :return: area
+    """
+    return math.pi * radius * radius
+
+def square():
+    """
+    Calculate square area and circumference
+    :param: none
+    :return: none
+    """
+    side = float(ask_side("Enter the length of the square's side: "))
+    circumference = 4 * side
+    area = side **2
+    print(f'The circumference is {circumference:.2f}')
+    print(f'The surface area is {area:.2f}')
+
+def rectangle():
+    """
+    Define rectangle
+    :param: none
+    :return: none
+    """
+    side1 = ask_side("Enter the length of the rectangle's side 1: ")
+    side2 = ask_side("Enter the length of the rectangle's side 2: ")
+    circumference = side1*2 + side2 * 2
+    area = side1 * side2
+    print(f'The circumference is {circumference:.2f}')
+    print(f'The surface area is {area:.2f}')
+
+def ask_side(text):
+    """
+    ask until positive
+    :param text: print text
+    :return: float, length of asked side
+    """
+    side = -1
+    while side < 0.00000001:
+        side = float(input(text))
+    return side
+
 
 def menu():
     """
@@ -13,12 +79,15 @@ def menu():
         if answer == "s":
             # Replace this comment and "pass" with your function calls dealing
             # with square.
-            pass
+            square()
 
         elif answer == "r":
             # Replace this comment and "pass" with your function calls dealing
             # with rectangle.
-            pass
+            rectangle()
+        
+        elif answer == 'c':
+            circle()
 
         elif answer == "q":
             return
